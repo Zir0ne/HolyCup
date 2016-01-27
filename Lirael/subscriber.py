a6fe27fe5af7fc8884b90e8661cbb13e50974a27
Zir0ne <Zir0ne@outlook.com>
2016-01-27 15:18:36 +0800

    1. 更新了工作方式, 改为由start和stop控制的工作线程模式

--- Lirael/subscriber.py
+++ Lirael/subscriber.py
@@ -5,45 +5,72 @@
 """
 
 import zmq
+import threading
 
 
 class Subscriber:
     """ 订阅者 """
 
-    def __init__(self, tick_filter=""):
+    def __init__(self, context, address, tick_filter=""):
         """ 构造函数
+            @param context     通信上下文, 进程唯一
+            @param address     发送tick信息的服务器的地址
             @param tick_filter 过滤器, 通过设置过滤器来滤掉不需要的tick信息
         """
-        self.filter  = tick_filter
-        self.context = None
-        self.socket  = None
+        self.filter   = tick_filter
+        self.context  = context
+        self.address  = address
+        self.socket   = None
+        self.handler  = None
+        self.quit_event = threading.Event()
+        self.quit_event.clear()
 
     def start(self, callback=None):
         """ 开始接收tick信息
-            @param callback 设置一个回调函数, 每次接收有用的tick信息后, 都会调用此函数
+            @param callback 设置一个回调函数, 每次接收有用的tick信息后, 都会调用此函数, 如果不提供, 仅仅打印tick信息
         """
         if callback and not hasattr(callback, "__call__"):
             print("%s cannot be invoked" % str(callback))
+            return
+        # 如果工作线程已经存在, 应当首先关闭, 再创建新的
+        if self.handler and not self.quit_event:
+            self.quit_event.set()
+            self.handler.join()
+        # 开启工作线程
+        self.quit_event.clear()
+        self.handler = threading.Thread(target=Subscriber.work_thread,
+                                        args=(None, self.context, self.address, self.quit_event, callback))
+        self.handler.start()
 
-        self.context = zmq.Context()
-        self.socket  = self.context.socket(zmq.SUB)
-        self.socket.connect("tcp://192.168.61.8:16888")
-        self.socket.setsockopt_string(zmq.SUBSCRIBE, '')
+    def stop(self):
+        """ 停止接收tick信息 """
+        if self.handler:
+            self.quit_event.set()
+            self.handler.join()
+            self.handler = None
+
+    def work_thread(self, *args):
+        """ 工作线程 """
+        # 准备socket
+        socket = args[0].socket(zmq.SUB)
+        socket.connect(args[1])
+        socket.setsockopt_string(zmq.SUBSCRIBE, '')
         print("Subscriber is collecting tick information......")
-
-        while True:
-            tick_info = self.socket.recv_string()
+        # 工作循环
+        quit_event = args[2]
+        callback   = args[3]
+        while not quit_event.is_set():
+            tick_info = socket.recv_string()
             if callback:
                 callback(tick_info)
             else:
                 print(tick_info)
-
-    def stop(self):
-        """ 停止接收tick信息 """
-        self.context.destroy()
+        # 退出, 清除资源
+        socket.close()
+        quit_event.clear()
         print("Subscriber has stopped, no more tick information will be collected.")
 
 
 if __name__ == "__main__":
-    sub = Subscriber(tick_filter="")
+    sub = Subscriber(zmq.Context(), "tcp://192.168.61.8:16888", tick_filter="")
     sub.start()
